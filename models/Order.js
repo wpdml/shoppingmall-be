b4f3121e7409e2e3d2cdb2f4d203d9f9c74f0ee2
이제의 <jeui@ije-uis-MacBook-Air.local>
2024-10-26 05:33:30 +0900

    ok

---
+++ models/Order.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = mongoose.Schema;
+const orderSchema = Schema(
+  {
+    userId: { type: mongoose.ObjectId, ref: User, required },
+    status: { type: String, default: "preparing" },
+    totalPrice: { type: Number, required: true, default: 0 },
+    shipTo: { type: Object, required: true },
+    contact: { type: Object, required: true },
+    orderNum: { type: String },
+    items: [
+      {
+        productId: { type: mongoose.ObjectId, ref: Product, required: true },
+        price: { type: Number, required: true },
+        size: { type: String, required: true },
+        qty: { type: Number, default: 1, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+cartSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updateAt;
+  return obj;
+};
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
